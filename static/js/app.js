ffe5c562a1ccaca7d4b43dbbc8e6398de8280571
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2018-11-30 11:26:06 -0700

    add handler and filter to data

--- static/js/app.js
+++ static/js/app.js
@@ -7,3 +7,23 @@ var submit = d3.select('#filter-btn');
 //select table body
 var tbody = d3.select('tbody');
 
+//click handler for datetime form
+submit.on('click', function() {
+
+    //prevent page refresh
+    d3.event.preventDefault();
+
+    //select input element and get raw HTML
+    var inputElem = d3.select('#datetime');
+
+    //get value property of the input
+    var inputVal = inputElem.property('value');
+
+    console.log(inputVal);
+    
+    //filter the data
+    var filtered = tableData.filter(ufo => ufo.datetime === inputVal);
+
+    console.log(filtered);
+
+});
